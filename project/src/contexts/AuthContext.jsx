7c12f11119f9106fe90a6998b052d2eb5fcaf723
Ravi Jithin Sai <raavijithinsai@gmail.com>
2025-12-10 19:44:14 +0530

    loginchanges

--- project/src/contexts/AuthContext.jsx
+++ project/src/contexts/AuthContext.jsx
@@ -10,7 +10,7 @@ export const AuthProvider = ({ children }) => {
 
   // Configure axios for session cookies
   axios.defaults.withCredentials = true;
-  axios.defaults.baseURL = "https://expireyeye.onrender.com";
+  axios.defaults.baseURL = "https://expireyeye.onrender.com/";
 
   // ✅ Check session when app starts
   useEffect(() => {
