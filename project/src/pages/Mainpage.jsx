6eb31fb0b16efef6823b724d076bd42d150f8ff3
Ravi Jithin Sai <raavijithinsai@gmail.com>
2026-05-06 15:23:51 +0530

    Fix case-sensitive import error for Kpicard

--- project/src/pages/Mainpage.jsx
+++ project/src/pages/Mainpage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { API_BASE_URL } from "../config";
-import KpiCard from './KpiCard';
+import KpiCard from './Kpicard';
 import Navbar from "./Navbar";
 import Charts from "./Chats";
 import AddProductModal from "../components/Addnewproduct";
